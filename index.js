2507f254131040e142bd689cb6f1ca5b71f65357
Travis Webb <me@traviswebb.com>
2014-07-09 11:41:47 -0400

    catch chmod error

--- index.js
+++ index.js
@@ -15,8 +15,14 @@ function fixPermissions () {
   var isRoot = process.getuid && process.getuid() === 0;
   if (isRoot) {
     // as in https://github.com/xtuple/xtuple-server/blob/070116365dff4dec4bfbb4345562dfd12c38c558/bootstrap.sh#L74
-    proc.execSync('chmod -Rf a+wr /usr/local/{share/systemtap,share/man,bin,lib/node*,include/node*,n*}',
-      { stdio: 'ignore' });
+    try {
+      var chmod = proc.execSync('chmod -Rf a+wr /usr/local/{share/systemtap,share/man,bin,lib/node*,include/node*,n*}',
+        { stdio: 'pipe' });
+    }
+    catch (e) {
+      log.verbose('n-api', 'error message', e.message);
+      log.verbose('n-api', 'chmod', chmod);
+    }
   }
 }
 
